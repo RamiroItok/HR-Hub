1c0a31f0e7ec0b3dcd1bcea240af9ab830d927a1
Ramiro Exequiel Itokazu <ramiro.itokazu@gmail.com>
2024-08-13 15:20:42 -0300

    EncriptacionService

---
+++ Aplication/Services/EncriptacionService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Aplication
+{
+    public class EncriptacionService
+    {
+        public static string IV = "qo1lc3sjd8zpt9cx"; //16 chars = 128 bytes
+        public static string key = "ow7dxys8glfor9tnc2ansdfo1etkfjcv";  // 32 chars = 256 bytes
+
+        public static string Encriptar_AES(string decrypted)
+        {
+            byte[] textbytes = ASCIIEncoding.ASCII.GetBytes(decrypted);
+            AesCryptoServiceProvider encdec = new AesCryptoServiceProvider();
+            encdec.BlockSize = 128;
+            encdec.KeySize = 256;
+            encdec.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            encdec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            encdec.Padding = PaddingMode.PKCS7;
+            encdec.Mode = CipherMode.CBC;
+
+            ICryptoTransform icrypt = encdec.CreateEncryptor(encdec.Key, encdec.IV);
+
+            byte[] enc = icrypt.TransformFinalBlock(textbytes, 0, textbytes.Length);
+            icrypt.Dispose();
+
+            return Convert.ToBase64String(enc);
+        }
+
+        public static string Decrypt_AES(string encrypted)
+        {
+            byte[] encbytes = Convert.FromBase64String(encrypted);
+            AesCryptoServiceProvider encdec = new AesCryptoServiceProvider();
+            encdec.BlockSize = 128;
+            encdec.KeySize = 256;
+            encdec.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            encdec.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            encdec.Padding = PaddingMode.PKCS7;
+            encdec.Mode = CipherMode.CBC;
+
+            ICryptoTransform icrypt = encdec.CreateDecryptor(encdec.Key, encdec.IV);
+
+            byte[] dec = icrypt.TransformFinalBlock(encbytes, 0, encbytes.Length);
+            icrypt.Dispose();
+
+            return ASCIIEncoding.ASCII.GetString(dec);
+        }
+
+        public static string Encriptar_MD5(string cadena)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(cadena);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            cadena = BitConverter.ToString(hash).Replace("-", "");
+            return cadena;
+        }
+    }
+}
---
+++ Aplication/Services/UsuarioService.cs
@@ -0,0 +1,48 @@
+﻿using Aplication.Interfaces;
+using Data.Interfaces;
+using Models;
+using System;
+
+namespace Aplication
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly IUsuarioDAO _usuarioDAO;
+
+        public UsuarioService(IUsuarioDAO usuarioDAO)
+        {
+            _usuarioDAO = usuarioDAO;
+        }
+
+        public int RegistrarUsuario(Usuario usuario)
+        {
+            try
+            {
+                /*Usuario usuarioReal = new Usuario()
+                {
+                    Nombre = EncriptacionService.Encriptar_AES(usuario.Nombre),
+                    Apellido = EncriptacionService.Encriptar_AES(usuario.Apellido),
+                    Email = EncriptacionService.Encriptar_AES(usuario.Email),
+                    Contraseña = EncriptacionService.Encriptar_MD5(usuario.Contraseña),
+                    Puesto = usuario.Puesto
+                };*/
+
+                Usuario usuarioReal = new Usuario()
+                {
+                    Nombre = usuario.Nombre,
+                    Apellido = usuario.Apellido,
+                    Email = usuario.Email,
+                    Contraseña = usuario.Contraseña,
+                    Puesto = usuario.Puesto
+                };
+
+                var idUsuario = _usuarioDAO.RegistrarUsuario(usuarioReal);
+                return idUsuario;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
